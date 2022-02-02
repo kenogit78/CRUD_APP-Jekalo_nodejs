54d847fc672e5041f034598df60dfa9b503e1de3
Paul Keno <adarojepaulo@gmail.com>
2022-02-02 19:55:14 +0100

    final final edits....

--- routes/index.js
+++ routes/index.js
@@ -5,7 +5,7 @@ const userRoutes = require('./api/user');
 
 
 router.get('/', (req, res) => {
-    return res.send('API is Running...')
+    return res.send('Jekalo Systems Solution Technical Assessment... Implemented by Adaroje Paul Keno')
 })
 
 router.use('/api', userRoutes);
